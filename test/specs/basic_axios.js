6c93f2c09dedf649b83f8593aba857d9dc9f102c
Alexander Kiyko <avkiyko@gmail.com>
2020-03-28 23:38:06 -0400

    initial commit

---
+++ test/specs/basic_axios.js
@@ -0,0 +1,14 @@
+import { expect } from 'chai';
+import axios from 'axios';
+
+describe('webdriver.io', () => {
+  it('should get success response',async () => {
+    const response = await axios({
+      method: 'get',
+      url: 'https://webdriver.io',
+    })
+      .then(res => res)
+      .catch(err => err);
+    expect(response.status).equal(200);
+  });
+});
